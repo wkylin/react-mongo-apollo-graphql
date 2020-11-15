b83942e3bd19c91947378ffc884a6668aca1c046
wkylin <wkylin.w@gmail.com>
2020-11-15 13:49:25 +0800

    get post by id

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -9,6 +9,19 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+    async getPost(_, { postId }, context, info) {
+      try {
+        const post = await Post.findById(postId);
+        if(post) {
+          return post;
+        } else {
+          throw new Error('post not fund');
+        }
+
+      } catch(err){
+        throw new Error(err);
+      }
     }
   }
 };
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -26,6 +26,7 @@ module.exports = gql`
 
   type Query {
     getPosts: [Post]
+    getPost(postId: ID!): Post
   }
   type Mutation {
     register(registerInput: RegisterInput): User!
